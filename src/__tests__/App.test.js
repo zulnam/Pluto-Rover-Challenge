3658700546072ebb6f5472f9bc40a410cf147ef7
Valentin Necsescu <valentin.necsescualpima.com@valentins-macbook-pro-2.home>
2022-05-03 19:03:17 +0200

    unitTests/corrected directional test to check correct direction change

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,6 +3,6 @@ import App from '../components/App';
 
 it('renders the main app', () => {
   const div = document.createElement('div');
-  const { container} = render(<App />, div);
+  const { container } = render(<App />, div);
   expect(container.firstChild).toMatchSnapshot();
 });
--- src/components/__tests__/CommandHistory.test.js
+++ src/components/__tests__/CommandHistory.test.js
@@ -2,19 +2,17 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../App';
 
 describe('Check Functionality of CommandHistory component', () => {
+  beforeEach(() => {
+    const div = document.createElement('div');
+    render(<App />, div);
+  });
 
-    beforeEach(() => {
-        const div = document.createElement('div');
-        render(<App />, div);
-    });
+  it('renders empty list on load', () => {
+    expect(screen.queryByTestId('logEntry')).toBeNull();
+  });
 
-    it('renders empty list on load', () => {
-        expect(screen.queryByTestId('logEntry')).toBeNull();
-      });
-
-    it('renders log entry on Forward command', () => {
-        fireEvent.click(screen.queryByTestId('forward'));
-        expect(screen.queryByTestId('logEntry')).toBeInTheDocument();
-    });
-
+  it('renders log entry on Forward command', () => {
+    fireEvent.click(screen.queryByTestId('forward'));
+    expect(screen.queryByTestId('logEntry')).toBeInTheDocument();
+  });
 });
